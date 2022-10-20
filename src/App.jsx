a77724ce89ac49bfe53d2aff240bafd2ae8fcd44
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-10-20 17:50:01 +0000

    added timer stop when hit 0

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,32 @@ import { useState, useEffect } from 'react';
 import Background from './components/Background';
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(60);
-  const [timerStatus, setTimerStatus] = useState(false);
-  const [length, setLength] = useState(0);
-  const [numPlayers, setNumPlayers] = useState(0);
+  const [currentTime, setCurrentTime] = useState(60); // The current state of the timer
+  const [timerStatus, setTimerStatus] = useState(false); // The current state of the timer (on/off)
+  const [length, setLength] = useState(0); // The length of the timer
+  const [numPlayers, setNumPlayers] = useState(0); // The total number of players
 
+  // sets the currentTimer whenever the length is set in the config component
  useEffect(() => {
     setCurrentTime(length);
   }, [length]);
 
 
+  // sets a interval to mimick a timer using the currentTime and timerStatus state
   useEffect(() => {
-    if (timerStatus === true) {
+    if (timerStatus === true && currentTime !== 0) { // checks if timer should be active using timerStatus & whether or not the timer is at 0
       const interval = setInterval(() => {
         setCurrentTime(currentTime - 1);
       }, 1000);
       return () => clearInterval(interval);
     }
   });
+
+    /* stops the timer before 0 */
+  useEffect(() => {
+    if (currentTime === 0)
+      setTimerStatus(false);
+  }, [currentTime]);
 
   return (
     <>
--- src/components/Race.jsx
+++ src/components/Race.jsx
@@ -6,18 +6,29 @@ import { contestantData } from '../assets/data';
 function Race(props) {
   const [contestants, setContestants] = useState(contestantData);
 
+    // Sets the contestants x position for every iteration of currentTime
   useEffect(() => {
-    if (props.timerStatus === true) {
-      let newContestantPos = contestants.map((contestant) => {
-        return {
-          ...contestant,
-          xpos: contestant.xpos + Math.floor(Math.random() * 10),
-        };
+    if (props.timerStatus === true && props.currentTime !== 0) {
+      let newContestantPos = contestants.map((contestant, i) => {
+        // IF CONTESTANT X POS IS LESS THAN 10 DONT RUN || ( IF CONTESTAN X POS IS GREATER THAN 10 )
+        if (contestant.xpos < 500) {
+          return {
+            ...contestant,
+            xpos: contestant.xpos + Math.floor(Math.random() * 30),
+          }
+        } else {
+        //console.log(contestant.xpos + Math.floor(Math.random() * 10) * (Math.random() * 5 < 2 ? -1 : 1))
+          return {
+            ...contestant,
+            xpos: contestant.xpos + Math.floor(Math.random() * 13) * (Math.random() < 0.5 ? -1 : 1),
+          }
+        }
       });
       setContestants(newContestantPos);
     }
   }, [props.currentTime]);
 
+    // Will only render the amount of contestants set by the state
   return (
     <div style={contestantStyles.contestantList}>
       {contestants.map((contestant, i) => (
